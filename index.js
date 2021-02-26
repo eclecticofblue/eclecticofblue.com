b12a73c42b3bdee2bdeeb51b4fe01549181974ce
HubTicaBB <tijana.ilic87@protonmail.com>
2021-02-26 10:33:22 +0100

    Init with base file structure

---
+++ index.js
@@ -0,0 +1,22 @@
+windows.onlo
+
+const showHome = () => {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("about").style.display = "none";
+    console.log("showHome");
+}
+
+const showFeatures = () => {
+    console.log("showFeatures");
+}
+
+const showAbout = () => {
+    document.getElementById("about").style.display = "block";
+    document.getElementById("home").style.display = "none";
+    console.log("showAbout");
+}
+
+const showContact = () => {
+    console.log("showContact");
+}
+
